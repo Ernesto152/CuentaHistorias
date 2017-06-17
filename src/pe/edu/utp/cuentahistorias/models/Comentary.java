61e833fa742a017a4098244c156373457f2f1c7f
will <0920076@utp.edu.pe>
2017-06-17 15:30:14 -0500

    qwe2

---
+++ src/pe/edu/utp/cuentahistorias/models/Comentary.java
@@ -0,0 +1,9 @@
+package pe.edu.utp.cuentahistorias.models;
+
+/**
+ * Created by will on 17/06/2017.
+ */
+public class Comentary {
+    String description;
+
+}
---
+++ src/pe/edu/utp/cuentahistorias/models/Subscription.java
@@ -0,0 +1,15 @@
+package pe.edu.utp.cuentahistorias.models;
+
+import java.util.Date;
+
+/**
+ * Created by will on 17/06/2017.
+ */
+public class Subscription {
+    int id;
+    String email;
+    int cellPhone;
+    Date registrationDate;
+    String password;
+
+}
