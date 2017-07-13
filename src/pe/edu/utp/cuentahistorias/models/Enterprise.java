cbd81bc8d1480f5faefef5612c7a33672f1c3045
Administrador <ljrf1493@gmai.com>
2017-07-13 13:13:02 -0500

    Enterprise

---
+++ src/pe/edu/utp/cuentahistorias/models/Enterprise.java
@@ -0,0 +1,48 @@
+package pe.edu.utp.cuentahistorias.models;
+
+/**
+ * Created by Administrador on 13/07/2017.
+ */
+public class Enterprise {
+    private int id;
+    private int ruc;
+    private String category;
+
+    public Enterprise() {
+    }
+
+    public Enterprise (int id, int ruc, String category) {
+
+        this.setId(id);
+        this.setRuc(ruc);
+        this.setCategory(category);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(int ruc) {
+        this.ruc = ruc;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+}
+
+
+
